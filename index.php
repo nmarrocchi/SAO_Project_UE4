7256f91e87775325f6ae67b661e3b501942d1775
nmarrocchi <nmarrocchi@la-providence.net>
2021-03-23 20:24:47 +0100

    Base Website

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="img/icon.png" type="image/x-icon">
+    <link rel="stylesheet" href="style/style.css">
+    <title>SAO Project UE4 - Accueil</title>
+</head>
+<body>
+
+    <div class="Banner">
+        <h1>Welcome into a new VRMMO Experience</h1>
+        <h4> Sword Art Online in Unreal Engine 4 </h4>
+    </div>
+    
+</body>
+</html>
